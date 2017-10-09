35a4fd2137640fb0c84f822f3d5f30ab24932899
ngkerry <31704416+ngkerry@users.noreply.github.com>
2017-10-08 22:52:38 -0700

    Create sketch-3.js

---
+++ sketch-3.js
@@ -0,0 +1,68 @@
+var circle = 
+{
+x: 100, 
+y: 400,
+vx: 0,
+vy: -5,
+r: 11
+};
+
+var circle2 = 
+{
+x: 150, 
+y: 400,
+vx: 0,
+vy: -6,
+r: 11
+};
+
+
+function setup() 
+{
+createCanvas(400, 400);
+	
+circle.h = random(360);
+circle2.h = random(360);
+}
+
+function draw() 
+{
+background(255);
+  
+paint(circle);
+paint(circle2);
+  
+move(circle);
+move(circle2);
+  
+bounce(circle);
+bounce(circle2);
+}
+
+function paint(circle) 
+{
+ellipse(circle.x, circle.y, circle.r*2, circle.r*2);
+	
+}
+
+function move(circle) 
+{
+circle.x += circle.vx;
+circle.y += circle.vy;
+}
+
+function bounce(circle) 
+{
+if (circle.x > width || circle.x < 0) 
+{
+circle.vx = circle.vx;
+}
+if (circle.y > height || circle.y < 0) 
+{
+circle.vy = circle.vy;
+}
+if (circle.y < 0)
+{
+circle.y = 400
+}
+}
